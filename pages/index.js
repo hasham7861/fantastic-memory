b7fea5dd277701d5295c7ff3e6d845fae25579c7
the-great-hasham <hasham@massively.ai>
2020-04-13 15:57:05 -0400

    added some props to canvas

--- pages/index.js
+++ pages/index.js
@@ -2,36 +2,26 @@
 import CanvasDraw from 'react-canvas-draw';
 import { useRef, useState } from 'react';
 
-
-const defaultProps = {
-    onChange: null,
-    loadTimeOffset: 5,
-    lazyRadius: 30,
-    brushRadius: 12,
-    brushColor: "#444",
-    catenaryColor: "#0a0302",
-    gridColor: "rgba(150,150,150,0.17)",
-    hideGrid: false,
-    canvasWidth: 400,
-    canvasHeight: 400,
-    disabled: true,
-    imgSrc: "",
-    saveData: null,
-    immediateLoading: false,
-    hideInterface: false
-};
-
-
-
 export default function () {
 
     let drawingCanvas = useRef(null);
 
     const [canvasOptions, setCanvasOptions] = useState({
-        brushRadius: "2px",
-        brushColor: "orange",
-        canvasWidth: "500px",
-        canvasHeight: "500px",
+        onChange: null,
+        loadTimeOffset: 5,
+        lazyRadius: 1,
+        brushRadius: 5,
+        brushColor: "#444",
+        catenaryColor: "#0a0302",
+        gridColor: "rgba(150,150,150,0.17)",
+        hideGrid: false,
+        canvasWidth: 400,
+        canvasHeight: 400,
+        disabled: false,
+        imgSrc: "",
+        saveData: null,
+        immediateLoading: false,
+        hideInterface: false
 
     })
 
