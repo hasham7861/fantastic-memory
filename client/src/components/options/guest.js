c2c6012284058f838309e2e8069074d1ec201343
Hasham Alam <hasham@MacBook-Pro.local>
2020-07-31 20:57:36 -0400

    added feedback text for guest to join

--- client/src/components/options/guest.js
+++ client/src/components/options/guest.js
@@ -1,25 +1,33 @@
-import React, {useState} from 'react';
-import {joinGame} from '../../services/game-sockets';
+import React, { useState, useRef } from 'react';
+import { joinGame } from '../../services/game-sockets';
 import './guest.css'
 export default function () {
-    
-    const [inputGameId, setInputGameId] = useState("");
 
-    const joinLobby = () =>{
+    const [inputGameId, setInputGameId] = useState("");
+    const joinGameDiv = useRef(null)
+    const waitingDiv = useRef(null)
+
+    const joinLobby = () => {
         joinGame(inputGameId)
+        if (inputGameId.length > 0) {
+            joinGameDiv.current.style.display = "none"
+            waitingDiv.current.style.display = "block"
+        }
 
     }
 
     return (
         <div id="guest">
             <p id="title">Sup Guest</p>
-            <p>To join a game please enter in gameId</p>
-            <input type="text" name="gameId" id="gameId" placeholder="gameid..." value={inputGameId} onChange={(e)=>setInputGameId(e.target.value)}/>
-            <button onClick={joinLobby}>Join Game</button>
+            <div ref={joinGameDiv}>
+                <p>To join a game please enter in gameId</p>
+                <input type="text" name="gameId" id="gameId" placeholder="gameid..." value={inputGameId} onChange={(e) => setInputGameId(e.target.value)} />
+                <button onClick={joinLobby}>Join Game</button>
+            </div>
+            <div ref={waitingDiv} style={{ display: "none" }}>
+                <p >Waiting on host to start the game</p>
+            </div>
         </div>
     )
 }
 
-const WaitingToJoin = function (){
-    // TODO: listen for the event to 
-}
