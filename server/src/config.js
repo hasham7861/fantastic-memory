a778fcb35fe098f0531ebfc112ea51b9ae4b3149
Hasham <hasham.alam@hotmail.com>
2021-07-18 15:45:30 -0400

    updated master config and git ignore

--- server/src/config.js
+++ server/src/config.js
@@ -2,5 +2,5 @@ export default {
     "PORT":5000,
     "AllowedOrigins": ["http://localhost:3000", "http://localhost:5000"],
     // WARNING: This dev db is exposed on purpose as this is a pen project
-    "DbDevUri":"mongodb+srv://fantastic_dev_user:vkoIvmz32ZXTVOQW@gartechcluster.yzune.mongodb.net/fantasy-memory-dev"
+    "DbDevUri":"mongodb+srv://fantastic_dev_user:Gr0Lc1I7G2dYb9XL@gartechcluster.yzune.mongodb.net/fantasy-memory-dev"
 }
