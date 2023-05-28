e9348e5716d57d80295a588b3f86c1edd00d0540
nitro <nitro@nitros-MacBook-Pro.local>
2023-05-28 11:33:34 -0400

    migrate to vite

---
+++ client/vite.config.js
@@ -0,0 +1,15 @@
+import { defineConfig } from 'vite'
+import reactRefresh from '@vitejs/plugin-react'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  // This changes the out put dir from dist to build
+  // comment this out if that isn't relevant for your project
+  build: {
+    outDir: 'build',
+  },
+  plugins: [
+    reactRefresh(),
+  ],
+  loader: {".js":"jsx"}
+})
